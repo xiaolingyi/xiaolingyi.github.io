85b82f2d09b7388a8eaff18defa32c44287198a4
user <user@fudan.edu.cn>
2022-01-19 11:25:43 +0800

    init

---
+++ compile.sh
@@ -0,0 +1,8 @@
+python2 jemdoc.py index.jemdoc
+python2 jemdoc.py publications.jemdoc
+python2 jemdoc.py collections.jemdoc
+python2 jemdoc.py teaching.jemdoc
+
+git add .
+git commit -m "init"
+git push
